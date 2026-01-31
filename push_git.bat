3b17704657813a54bd6f174588037569ebc735f5
Aquaponics User <user@local>
2026-02-01 03:30:34 +0700

    chore: Remove Git utility batch scripts.

--- push_git.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/Chanakun22/AquaponicPJ.git
-IF %ERRORLEVEL% NEQ 0 (
-  "C:\Program Files\Git\cmd\git.exe" remote set-url origin https://github.com/Chanakun22/AquaponicPJ.git
-)
-"C:\Program Files\Git\cmd\git.exe" branch -M main
-"C:\Program Files\Git\cmd\git.exe" push -u origin main
--- setup_git.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-"C:\Program Files\Git\cmd\git.exe" init
-"C:\Program Files\Git\cmd\git.exe" config user.name "Aquaponics User"
-"C:\Program Files\Git\cmd\git.exe" config user.email "user@local"
-"C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit with secured credentials"
-"C:\Program Files\Git\cmd\git.exe" branch -M main
-echo GIT SETUP COMPLETE
